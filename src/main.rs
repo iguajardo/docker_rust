c456f4ba84366264292053942834205815c6421d
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-28 21:00:44 -0300

    second test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::Stdio;
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() {
     let args: Vec<_> = std::env::args().collect();
@@ -5,13 +7,17 @@ fn main() {
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .output()
         .unwrap();
 
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout).unwrap();
-        println!("{}", std_out)
+        println!("{}", std_out);
     } else {
+        let std_err = std::str::from_utf8(&output.stderr).unwrap();
+        println!("{}", std_err);
         std::process::exit(1);
     }
 }
