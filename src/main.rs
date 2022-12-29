4ebf77fccaa8963074d0b96d176feef7fe71deff
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-28 21:04:50 -0300

    fourth try second test

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,12 @@ fn main() {
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .output()
         .unwrap();
 
     if output.status.success() {
-        let std_out = String::from_utf8_lossy(&output.stdout);
-        print!("{}", std_out);
-    } else {
-        let std_err = String::from_utf8_lossy(&output.stderr);
-        print!("{}", std_err);
-        std::process::exit(1);
+        std::process::exit(0);
     }
 }
