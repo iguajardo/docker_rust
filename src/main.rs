620de2f24f84d4fa4f21e9d885c8e0946ab48892
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-29 21:23:55 -0300

    2nd try 3rd test

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@ fn main() {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let status = std::process::Command::new(command)
+    let output = std::process::Command::new(command)
         .args(command_args)
         .stdout(Stdio::inherit()) // Stdio::piped() // not needed if .output exists
         .stderr(Stdio::inherit()) // Stdio::piped()
-        .status()
+        .output()
         .unwrap();
 
-    if status.success() {
+    let exit_status = output.status;
+
+    if output.status.success() {
         // let std_out = std::str::from_utf8(&output.std_out);
         // print!({}, std_out);
         // let std_err = std::str::from_utf8(&output.std_err);
         // eprint!({}, std_err);
-        std::process::exit(status.code().unwrap());
+        std::process::exit(exit_status.code().unwrap());
     }
 }
