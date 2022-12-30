bba76c31ffd38bce1709dfb1c302c2d0d8b333f3
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-29 21:17:10 -0300

    3rd test

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@ fn main() {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = std::process::Command::new(command)
+    let status = std::process::Command::new(command)
         .args(command_args)
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
+        .stdout(Stdio::inherit()) // Stdio::piped() // not needed if .output exists
+        .stderr(Stdio::inherit()) // Stdio::piped()
+        .status()
         .unwrap();
 
-    if output.status.success() {
+    if status.success() {
         // let std_out = std::str::from_utf8(&output.std_out);
         // print!({}, std_out);
         // let std_err = std::str::from_utf8(&output.std_err);
         // eprint!({}, std_err);
-        std::process::exit(0);
+        std::process::exit(status.code().unwrap());
     }
 }
